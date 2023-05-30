fbe14d5e2e035b21c9aa53de4e148eb24fc29bfe
salvarenga25 <71409222+salvarenga25@users.noreply.github.com>
2023-05-30 13:28:35 -0400

    Added services and pages plus signup pg

---
+++ src/app/classes/cliente.ts
@@ -0,0 +1,9 @@
+export class Cliente {
+    indice:number;
+    razon:String;
+    tipodoc:String;
+    dnioruc:String;
+    direccion:String;
+    celular:String;
+    email:string;
+}
---
+++ src/app/components/lista-clientes/lista-clientes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-clientes',
+  templateUrl: './lista-clientes.component.html',
+  styleUrls: ['./lista-clientes.component.css']
+})
+export class ListaClientesComponent {
+  constructor(){ }
+
+  ngOnInit(): void {
+  }
+}
