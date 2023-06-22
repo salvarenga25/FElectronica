a1ddc32e3fb3fb5acf86de1fa4ca6134b2032bd1
salvarenga25 <71409222+salvarenga25@users.noreply.github.com>
2023-06-22 01:18:45 -0400

    Update dashboard, etc

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginData={
+  "username" : '',
+  "password" : ''
+  }
+  constructor(private snack:MatSnackBar , private loginService:LoginService){ }
 
+  ngOnInit(): void{
+
+  }
+
+  formSubmit(){
+    if(this.loginData.username.trim() == '' || this.loginData.username.trim() == null){
+      this.snack.open('El nombre de usuario es requerido !!', 'Aceptar',{
+        duration:3000,
+        verticalPosition:'top',
+        horizontalPosition:'right'
+      })
+      return;
+    }
+    if(this.loginData.password.trim() == '' || this.loginData.password.trim() == null){
+      this.snack.open('La contraseña es requerida !!', 'Aceptar',{
+        duration:3000,
+        verticalPosition:'top',
+        horizontalPosition:'right'
+      })
+      return;
+    }
+
+    this.loginService.generateToken(this.loginData).subscribe(
+      (dato:any) =>{
+        console.log(dato);
+        this.loginService.loginUser(dato.token);
+        this.loginService
+      },(error) =>{
+        console.log(error);
+        this.snack.open('Datos Erroneos, Intente otra vez', 'Aceptar',{
+          duration:3000
+        });
+      } 
+    )
+  }
 }
